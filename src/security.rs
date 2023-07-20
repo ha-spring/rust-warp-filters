2a35f67e21ff95e7dd84a0021fce2b3ed565bdc5
ha-spring <spring4boot@gmail.com>
2023-07-20 13:35:55 +0000

    add check_auth filter

---
+++ src/security.rs
@@ -0,0 +1,21 @@
+use warp::Filter;
+
+const HEADER_XAUTH: &str = "X-Auth-Token";
+
+pub fn check_auth() -> impl Filter<Extract = ((),), Error = warp::Rejection> + Clone {
+    warp::any()
+        .and(warp::header::<String>(HEADER_XAUTH))
+        .and_then(|xauth: String| async move {
+            // Check auth
+            if !xauth.ends_with("test") {
+                return Err(warp::reject::custom(FailAuth));
+            }
+
+            Ok::<(), warp::Rejection>(())
+        })
+}
+
+#[derive(Debug)]
+pub struct FailAuth;
+
+impl warp::reject::Reject for FailAuth {}
--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 curl -s http://localhost:8080/
 curl -s http://localhost:8080/index.html | grep 'Test file' | tr -d ' '
-curl -s http://localhost:8080/todos | jq -c '.[0]'
-curl -s http://localhost:8080/todos/123 | jq -c '.'
-curl -s -X POST http://localhost:8080/todos -H "Content-Type: application/json" -d '{"id": 34, "title": "todo 34"}'
+curl -s -H "X-Auth-Token: test" http://localhost:8080/todos | jq -c '.[0]'
+curl -s -H "X-Auth-Token: test" http://localhost:8080/todos/123 | jq -c '.'
+curl -s -X POST http://localhost:8080/todos -H "Content-Type: application/json" -H "X-Auth-Token: test" -d '{"id": 34, "title": "todo 34"}'
+echo
+echo "=== Errors ==="
+curl -s http://localhost:8080/todos/123
+echo
+curl -s -H "X-Auth-Token: test000" http://localhost:8080/todos/123
